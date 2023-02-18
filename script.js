75f6911a744b0a3190735b69b371ada13b53f2e2
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-02-19 00:10:59 +0900

    time 표시

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const messageContainer = document.querySelector("#d-day-message");
-messageContainer.textContent = "D-Day를 입력해주세요.";
+const container = document.querySelector("#d-day-container");
+
+// container.style.display = "none";
+messageContainer.innerHTML = "<h3>D-Day를 입력해주세요.</h3>";
 
 const dateFormMaker = () => {
     const inputYear = document.querySelector("#target-year-input").value;
@@ -13,18 +16,31 @@ const counterMaker = () => {
     const targetDateInput = dateFormMaker();
     const nowDate = new Date();
     const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
-
     const remaining = (targetDate - nowDate) / 1000;
-    const remainingDate = Math.floor(remaining / 3600 / 24);
-    const remainingHours = Math.floor(remaining / 3600) % 24;
-    const remainingMin = Math.floor(remaining / 60) % 60;
-    const remainingSec = Math.floor(remaining) % 60;
+
+    const remainingObj = {
+        remainingDate: Math.floor(remaining / 3600 / 24),
+        remainingHours: Math.floor(remaining / 3600) % 24,
+        remainingMin: Math.floor(remaining / 60) % 60,
+        remainingSec: Math.floor(remaining) % 60,
+    };
+    const documentObj = {
+        days: document.getElementById("days"),
+        hours: document.getElementById("hours"),
+        min: document.getElementById("min"),
+        sec: document.getElementById("sec"),
+    };
+
+    documentObj["days"].textContent = remainingObj["remainingDate"];
+    documentObj["hours"].textContent = remainingObj["remainingHours"];
+    documentObj["min"].textContent = remainingObj["remainingMin"];
+    documentObj["sec"].textContent = remainingObj["remainingSec"];
 
     if (remaining <= 0) {
         console.log("타이머 종료!");
+        messageContainer.innerHTML = "<h3>타이머가 종료됐습니다.</h3>";
     } else if (isNaN(remaining)) {
         console.log("유효한 날짜가 아닙니다.");
+        messageContainer.innerHTML = "<h3>유효한 날짜가 아닙니다.</h3>";
     }
-
-    console.log(remainingDate, remainingHours, remainingMin, remainingSec);
 };
